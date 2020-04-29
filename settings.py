9cdc7b914b01496f659c3e5f0d3dc84a3fb9f1a8
JoeCare <53105042+JoeCare@users.noreply.github.com>
2020-04-29 15:04:04 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,13 +7,8 @@ app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(BASE_DIR, 'database.db')
-app.config['FLASK_ENV'] = 'development'
-# or: 2. with absolute path
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/morfina/PycharmProjects/P12FlApi/database.db'
-
-# number of :/ is the key...
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CSRF_ENABLED'] = True
-app.config['CSRF_SESSION_KEY'] = "43g4vh368ds58!645fdg583t76e"
-app.config['SECRET_KEY'] = "345fdghg4vh368ds5f!45fdg"
+app.config['CSRF_SESSION_KEY'] = "flapi"
+app.config['SECRET_KEY'] = "flapi"
